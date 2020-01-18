c0a75fb565a23224d586fb4db5a10b4e805a174d
KJFreys1 <kfreyermuth20@gmail.com>
2020-01-18 14:32:57 -0800

    added some about content

--- blog/src/components/About.js
+++ blog/src/components/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function About() {
     return (
         <div className='about-container'>
-            <h1>This is the about page</h1>
+            <h1>About Page</h1>
+            <p>This app is developed with React using a data.json file to pull content in and display them on the blog page.</p>
         </div>
     )
 }
